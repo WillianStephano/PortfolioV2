150d0ec40d6a338d4d535d2d0318330f22fb9abd
“Willian <will.stephano@gmail.com>
2023-06-19 14:29:35 -0300

    Criação de seção intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { IntroComponent } from './componentes/intro/intro.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, CabecalhoComponent, IntroComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
